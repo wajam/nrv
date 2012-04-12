8c86eca47567c26a00b1b252ed0324ccd5952e75
Andre-Philippe Paquet <appaquet@gmail.com>
2012-04-12 15:15:27 -0400

    Changing sync method

--- nrv-core/src/main/scala/com/wajam/nrv/Logging.scala
+++ nrv-core/src/main/scala/com/wajam/nrv/Logging.scala
@@ -8,12 +8,12 @@ import org.slf4j.LoggerFactory
 trait Logging {
   val log = LoggerFactory.getLogger(getClass)
 
-  def debug(msg: => String, params: AnyRef*) = if (log.isDebugEnabled) log.debug(msg, params)
+  def debug(msg: => String, params: AnyRef*) = if (log.isDebugEnabled) log.debug(msg, params.toArray)
 
-  def info(msg: => String, params: AnyRef*) = if (log.isInfoEnabled) log.info(msg, params)
+  def info(msg: => String, params: AnyRef*) = if (log.isInfoEnabled) log.info(msg, params.toArray)
 
-  def warn(msg: => String, params: AnyRef*) = if (log.isWarnEnabled) log.warn(msg, params)
+  def warn(msg: => String, params: AnyRef*) = if (log.isWarnEnabled) log.warn(msg, params.toArray)
 
-  def error(msg: => String, params: AnyRef*) = if (log.isErrorEnabled) log.error(msg, params)
+  def error(msg: => String, params: AnyRef*) = if (log.isErrorEnabled) log.error(msg, params.toArray)
 
 }
