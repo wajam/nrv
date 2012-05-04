3cc95ef1649be2084b7833e8f6ced33ebd739eb5
felix <felix@wajam.com>
2012-05-04 15:13:33 -0400

    increased slf4j version

--- project/Project.scala
+++ project/Project.scala
@@ -12,13 +12,12 @@ object NrvBuild extends Build {
   )
 
   var commonDeps = Seq(
-    "org.slf4j" % "slf4j-nop" % "1.5.8",
+    "org.slf4j" % "slf4j-nop" % "1.6.4",
     "com.yammer.metrics" %% "metrics-scala" % "2.1.2",
     "io.netty" % "netty" % "3.4.2.Final" withSources(),
     "org.scalatest" %% "scalatest" % "1.7.1" % "test,it",
     "junit" % "junit" % "4.10" % "test,it",
     "org.mockito" % "mockito-core" % "1.9.0" % "test,it"
-
   )
 
   var zookeeperDeps = Seq(
