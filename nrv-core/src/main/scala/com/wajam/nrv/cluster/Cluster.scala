5e7c101e901a2692bf97259a31ff079c56c721ee
felix <felix@wajam.com>
2012-05-17 12:54:10 -0400

    added logging

--- nrv-core/src/main/scala/com/wajam/nrv/cluster/Cluster.scala
+++ nrv-core/src/main/scala/com/wajam/nrv/cluster/Cluster.scala
@@ -30,9 +30,11 @@ with ProtocolMessageListener with Logging {
   def messageReceived(inMessage: InMessage) {
     val action = cluster.getAction(inMessage.actionURL, inMessage.method)
     if (action != null) {
+      info("Received an incoming message for path {} with method {}.",
+        inMessage.actionURL.toString, inMessage.method.toString)
       action.callIncomingHandlers(inMessage)
     } else {
-      warn("Received a incoming for path {}, but couldn't find action", inMessage.actionURL.toString)
+      warn("Received an incoming for path {}, but couldn't find action", inMessage.actionURL.toString)
       throw new ListenerException("Route not found.")
     }
   }
