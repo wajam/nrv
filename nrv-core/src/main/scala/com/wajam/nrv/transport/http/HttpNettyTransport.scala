0accb657e8d9a9fc862b7af7a5b2641ce9ec94e3
felixtrepanier <felixtrepanier@gmail.com>
2012-07-11 23:08:32 -0400

    added status code metrics

--- nrv-core/src/main/scala/com/wajam/nrv/transport/http/HttpNettyTransport.scala
+++ nrv-core/src/main/scala/com/wajam/nrv/transport/http/HttpNettyTransport.scala
@@ -3,8 +3,12 @@ package com.wajam.nrv.transport.http
 import java.net.InetAddress
 import com.wajam.nrv.protocol.Protocol
 import com.wajam.nrv.transport.netty.{NettyTransportCodecFactory, NettyTransport}
-import org.jboss.netty.channel.ChannelPipeline
+import org.jboss.netty.channel.{Channel, ChannelHandlerContext, ChannelPipeline}
 import org.jboss.netty.handler.codec.http._
+import com.yammer.metrics.scala.Instrumented
+import org.jboss.netty.buffer.ChannelBuffer
+import org.jboss.netty.handler.codec.http.HttpMessageDecoder.State
+import org.jboss.netty.handler.codec.oneone.{OneToOneDecoder, OneToOneEncoder}
 
 /**
  * This class...
@@ -16,25 +20,68 @@ import org.jboss.netty.handler.codec.http._
 class HttpNettyTransport(host: InetAddress, port: Int, protocol: Protocol)
   extends NettyTransport(host, port, protocol) {
 
-  val factory = HttpNettyTransportCodecFactory
-}
+  val factory = new HttpNettyTransportCodecFactory
 
-object HttpNettyTransportCodecFactory extends NettyTransportCodecFactory {
+  class HttpNettyTransportCodecFactory extends NettyTransportCodecFactory {
 
-  def configureRequestEncoders(pipeline: ChannelPipeline) {
-    pipeline.addLast("encoder", new HttpRequestEncoder())
+    def configureRequestEncoders(pipeline: ChannelPipeline) {
+      pipeline.addLast("encoder", new HttpRequestEncoder())
+    }
+
+    def configureResponseEncoders(pipeline: ChannelPipeline) {
+      pipeline.addLast("encoder", new HttpResponseEncoder())
+      pipeline.addLast("deflater", new HttpContentCompressor())
+      pipeline.addLast("metrics", new ServerHttpResponseMetricUpdater())
+    }
+
+    def configureRequestDecoders(pipeline: ChannelPipeline) {
+      pipeline.addLast("decoder", new HttpRequestDecoder())
+    }
+
+    def configureResponseDecoders(pipeline: ChannelPipeline) {
+      pipeline.addLast("decoder", new HttpResponseDecoder())
+      pipeline.addLast("metrics", new ClientHttpResponseMetricUpdater())
+    }
   }
 
-  def configureResponseEncoders(pipeline: ChannelPipeline) {
-    pipeline.addLast("encoder",  new HttpResponseEncoder())
-    pipeline.addLast("deflater", new HttpContentCompressor())
+  class ServerHttpResponseMetricUpdater extends OneToOneEncoder{
+    val updater = new HttpResponseMetricUpdater("server")
+
+    override def encode(channelContext: ChannelHandlerContext, channel: Channel, msg: AnyRef) = {
+      msg match {
+        case response: HttpResponse => updater.updateCounter(response)
+        case _ =>
+      }
+      msg
+    }
   }
 
-  def configureRequestDecoders(pipeline: ChannelPipeline) {
-    pipeline.addLast("decoder", new HttpRequestDecoder())
+  class ClientHttpResponseMetricUpdater extends OneToOneDecoder{
+    val updater = new HttpResponseMetricUpdater("client")
+
+    def decode(channelContext: ChannelHandlerContext, channel: Channel, msg: AnyRef) = {
+      msg match {
+        case response: HttpResponse => updater.updateCounter(response)
+        case _ =>
+      }
+      msg
+    }
   }
 
-  def configureResponseDecoders(pipeline: ChannelPipeline) {
-    pipeline.addLast("decoder",  new HttpResponseDecoder())
+  class HttpResponseMetricUpdater(prefix: String) extends Instrumented {
+
+    val success = metrics.counter(prefix+"-http-2xx")
+    val clientFailure = metrics.counter(prefix+"-http-4xx")
+    val serverFailure = metrics.counter(prefix+"-http-5xx")
+    val other = metrics.counter(prefix+"-http-other")
+
+    def updateCounter(response: HttpResponse) {
+      response.getStatus.getCode / 100 match {
+        case 2 => success += 1
+        case 4 => clientFailure += 1
+        case 5 => serverFailure += 1
+        case _ => other += 1
+      }
+    }
   }
 }
