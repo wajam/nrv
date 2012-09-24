e2ccc0d68995dcbed840e3599f5cfaa4802a479e
Manuel Laflamme <manuel@wajam.com>
2012-09-24 16:10:01 -0400

    Improve the Traced trait to make its usage seemless with Yammer Timer.

--- nrv-core/src/main/scala/com/wajam/nrv/tracing/Traced.scala
+++ nrv-core/src/main/scala/com/wajam/nrv/tracing/Traced.scala
@@ -3,30 +3,54 @@ package com.wajam.nrv.tracing
 import com.yammer.metrics.core.MetricName
 import java.util.concurrent.TimeUnit
 import com.yammer.metrics.scala.{Instrumented, Timer}
+import com.wajam.nrv.tracing.Annotation.Message
 
 /**
  * The mixin trait for creating a class which is traced with NRV tracing and instrumented with metrics.
  */
-trait Traced {
-  def tracedTimer(name: String, instrumented: Instrumented, scope: String = null,
+trait Traced extends Instrumented {
+
+  def tracedTimer(name: String, scope: String = null,
                   durationUnit: TimeUnit = TimeUnit.MILLISECONDS, rateUnit: TimeUnit = TimeUnit.SECONDS) = {
-    new TracedTimer(new MetricName(instrumented.getClass, name, scope),
-      instrumented.metrics.timer(name, scope, durationUnit, rateUnit, instrumented.metrics.metricsRegistry))
+    new TracedTimer(metrics.timer(name, scope, durationUnit, rateUnit, metrics.metricsRegistry),
+      new MetricName(getClass, name, scope))
   }
 }
 
-class TracedTimer(name: MetricName, timer: Timer) {
+/**
+ * Wrapper for Yammer metrics Timer
+ */
+class TracedTimer(val timer: Timer, val name: String, val source: Option[String]) {
 
-  def time[S](tracer: Option[Tracer])(block: => S) {
+  def this(timer: Timer, name: MetricName) {
+    this(timer, name.getName, Some(name.getGroup + "." + name.getType))
+  }
+
+  /**
+   * Runs block, recording its duration, and returns the result of block.
+   */
+  def time[S](block: => S): S = {
+    val tracer = Tracer.currentTracer
     timer.time {
       if (tracer.isDefined) {
-        tracer.get.time(name.toString) {
+        tracer.get.time(name, source) {
           block
         }
       } else {
         block
       }
     }
+  }
+
+  /**
+   * Adds a recorded duration.
+   */
+  def update(duration: Long, unit: TimeUnit) {
+    val tracer = Tracer.currentTracer
+    if (tracer.isDefined) {
+      tracer.get.record(Message(name, source), Some(unit.toMillis(duration)))
+    }
+    timer.update(duration, unit)
   }
 
 }
