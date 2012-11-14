5e41fa47dd57b6adc616338f3551f94262591058
Manuel Laflamme <manuel@wajam.com>
2012-11-14 16:34:28 -0500

    Fix TransformLogging varargs params (missing splat)

--- nrv-core/src/main/scala/com/wajam/nrv/utils/TransformLogging.scala
+++ nrv-core/src/main/scala/com/wajam/nrv/utils/TransformLogging.scala
@@ -11,35 +11,35 @@ trait TransformLogging extends Logging {
   override def debug(msg: => String, params: Any*) {
     if (log.isDebugEnabled) {
       val (transformedMsg, transformedParams) = transformLogMessage(msg, params)
-      super.debug(transformedMsg, transformedParams)
+      super.debug(transformedMsg, transformedParams: _*)
     }
   }
 
   override def trace(msg: => String, params: Any*) {
     if (log.isTraceEnabled) {
       val (transformedMsg, transformedParams) = transformLogMessage(msg, params)
-      super.trace(transformedMsg, transformedParams)
+      super.trace(transformedMsg, transformedParams: _*)
     }
   }
 
   override def info(msg: => String, params: Any*) {
     if (log.isInfoEnabled) {
       val (transformedMsg, transformedParams) = transformLogMessage(msg, params)
-      super.info(transformedMsg, transformedParams)
+      super.info(transformedMsg, transformedParams: _*)
     }
   }
 
   override def warn(msg: => String, params: Any*) {
     if (log.isWarnEnabled) {
       val (transformedMsg, transformedParams) = transformLogMessage(msg, params)
-      super.warn(transformedMsg, transformedParams)
+      super.warn(transformedMsg, transformedParams: _*)
     }
   }
 
   override def error(msg: => String, params: Any*) {
     if (log.isErrorEnabled) {
       val (transformedMsg, transformedParams) = transformLogMessage(msg, params)
-      super.error(transformedMsg, transformedParams)
+      super.error(transformedMsg, transformedParams: _*)
     }
   }
 
