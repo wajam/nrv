d5daa720e7235a7a78085e2c51680dd49feb3b4b
Manuel Laflamme <manuel@wajam.com>
2013-01-08 10:13:43 -0500

    Moved Timestamp trait from Scn to Nrv

---
+++ nrv-core/src/main/scala/com/wajam/nrv/consistency/ConsistencyManager.scala
@@ -0,0 +1,12 @@
+package com.wajam.nrv.consistency
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: manuel
+ * Date: 08/01/13
+ * Time: 9:28 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class ConsistencyManager {
+
+}
