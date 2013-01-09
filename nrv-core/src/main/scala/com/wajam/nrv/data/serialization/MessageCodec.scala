83b246eb3bf1eb81bdadfd65eba962c2b381081e
Olivier Toupin <olivier.toupin@wajam.com>
2013-01-09 17:11:41 -0500

    New rename

--- nrv-core/src/main/scala/com/wajam/nrv/data/serialization/MessageCodec.scala
+++ nrv-core/src/main/scala/com/wajam/nrv/data/serialization/MessageCodec.scala
@@ -1,12 +1,13 @@
 package com.wajam.nrv.data.serialization
 
 import com.wajam.nrv.data.Message
+import com.wajam.nrv.data.serialization.ProtoBufMessage;
 
 class MessageCodec {
 
   def encode(message: Message) = {
 
-
+    ProtobufMessage.ProtoBufMessage.Builder
   }
 
   def decode(data: Array[Byte]) = {
