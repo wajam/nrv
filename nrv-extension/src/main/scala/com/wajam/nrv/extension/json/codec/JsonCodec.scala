c50e04b7081f16deb98d7ada6431c1804edc055d
Manuel Laflamme <manuel@wajam.com>
2013-01-18 10:19:15 -0500

    Support sequence in TaskContext persistence

--- nrv-extension/src/main/scala/com/wajam/nrv/extension/json/codec/JsonCodec.scala
+++ nrv-extension/src/main/scala/com/wajam/nrv/extension/json/codec/JsonCodec.scala
@@ -9,18 +9,27 @@ import net.liftweb.json._
 
 class JsonCodec extends Codec {
 
-  def encode(json: Any, context: Any): Array[Byte] = {
+  def encode(data: Any, context: Any): Array[Byte] = {
+    import JsonCodec._
+
     val contentEncoding = context.asInstanceOf[String]
-    json match {
+    data match {
       case s: String => s.getBytes(contentEncoding)
       case l: Seq[Any] => compact(render(toJValue(l))).getBytes(contentEncoding)
       case m: Map[String, Any] => compact(render(toJValue(m))).getBytes(contentEncoding)
       case v: JValue => compact(render(v)).getBytes(contentEncoding)
-      case _ => throw new RuntimeException("Invalid type, can not render json for " + json.getClass)
+      case _ => throw new RuntimeException("Invalid type, can not render json for " + data.getClass)
     }
   }
 
-  private def toJValue(value: Any): JValue = {
+  def decode(data: Array[Byte], context: Any) = {
+    val contentEncoding = context.asInstanceOf[String]
+    parse(new String(data, contentEncoding))
+  }
+}
+
+object JsonCodec {
+  def toJValue(value: Any): JValue = {
     value match {
       case v: JValue => v
       case s: String => JString(s)
@@ -32,10 +41,5 @@ class JsonCodec extends Codec {
       case map: Map[String, Any] => JObject(map.map(e => JField(e._1, toJValue(e._2))).toList)
       case _ => throw new RuntimeException("Invalid type, can not render json for " + value.getClass)
     }
-  }
-
-  def decode(data: Array[Byte], context: Any) = {
-    val contentEncoding = context.asInstanceOf[String]
-    parse(new String(data, contentEncoding))
   }
 }
