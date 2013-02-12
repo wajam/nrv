51dc41146a5f5030ed0caacd5a3f28f660293c06
Manuel Laflamme <manuel@wajam.com>
2013-02-12 11:11:12 -0500

    Change trace scribe channel from "trace" to "traces"

--- nrv-scribe/src/main/scala/com/wajam/nrv/scribe/ScribeTraceRecorder.scala
+++ nrv-scribe/src/main/scala/com/wajam/nrv/scribe/ScribeTraceRecorder.scala
@@ -6,7 +6,7 @@ import com.wajam.nrv.tracing.{TraceRecordFormatter, Record, TraceRecorder}
  * Scribe trace recorder.
  */
 class ScribeTraceRecorder(scribeHost: String, scribePort: Int, samplingRate: Int) extends TraceRecorder {
-  private val scribeClient = new ScribeClient("trace", scribeHost, scribePort)
+  private val scribeClient = new ScribeClient("traces", scribeHost, scribePort)
   scribeClient.start()
 
   def record(record: Record) {
