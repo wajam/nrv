50853204a2a8b0b5337c619b23f21cb24b4c5c51
Manuel Laflamme <manuel@wajam.com>
2013-02-12 13:55:46 -0500

    Queued and ordered transaction recording (WIP)

--- nrv-core/src/main/scala/com/wajam/nrv/consistency/Consistency.scala
+++ nrv-core/src/main/scala/com/wajam/nrv/consistency/Consistency.scala
@@ -3,6 +3,8 @@ package com.wajam.nrv.consistency
 import com.wajam.nrv.service.{MessageHandler, Service}
 import com.wajam.nrv.utils.{VotableEvent, Event}
 import com.wajam.nrv.cluster.Cluster
+import com.wajam.nrv.utils.timestamp.Timestamp
+import com.wajam.nrv.data.Message
 
 /**
  * Manage consistency inside the cluster based on events
@@ -31,5 +33,20 @@ abstract class Consistency extends MessageHandler {
       case _ =>
     }
   }
+}
 
+object Consistency {
+  val TimestampHeader = "X-CONSISTENCY-TS"
+
+  def setMessageTimestamp(message: Message, timestamp: Timestamp) {
+    message.attachments(TimestampHeader) = timestamp
+  }
+
+  def getMessageTimestamp(message: Message): Option[Timestamp] = {
+    message.attachments.get(TimestampHeader) match {
+      case Some(ts: Timestamp) => Some(ts)
+      case Some(ts) => Some(Timestamp(ts.toString.toLong))
+      case _ => None
+    }
+  }
 }
