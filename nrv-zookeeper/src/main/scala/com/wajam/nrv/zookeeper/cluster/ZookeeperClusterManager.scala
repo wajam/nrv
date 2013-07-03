4c74df08298b6ce923131b2db12dd20c45201122
jnblanchet <jean-nicola@wajam.com>
2013-07-03 09:26:11 -0400

    fixed shard move bug (race) that caused two shards to be merged into a single one for a short period of time during a shard move

--- nrv-zookeeper/src/main/scala/com/wajam/nrv/zookeeper/cluster/ZookeeperClusterManager.scala
+++ nrv-zookeeper/src/main/scala/com/wajam/nrv/zookeeper/cluster/ZookeeperClusterManager.scala
@@ -145,7 +145,7 @@ class ZookeeperClusterManager(val zk: ZookeeperClient) extends DynamicClusterMan
         val path = ZookeeperClusterManager.zkMemberVotePath(service.name, oldServiceMember.token, oldServiceMember.token)
         zk.delete(path)
       } catch {
-        case e: NoNodeException => //data has already been deleted (e.g. entries are deleted manually in ZKclusterManager it tests)
+        case e: NoNodeException => // data has already been deleted (e.g. entries are deleted manually in ZKclusterManager it tests)
       }
     }
   }
