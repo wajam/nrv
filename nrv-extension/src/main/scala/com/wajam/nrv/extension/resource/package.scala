7232b0e6695b351388d732a585ad38e71f20bd3d
Manuel Laflamme <manuel@waj.am>
2013-09-25 15:47:40 -0400

    More exhaustive Request param extractors

--- nrv-extension/src/main/scala/com/wajam/nrv/extension/resource/package.scala
+++ nrv-extension/src/main/scala/com/wajam/nrv/extension/resource/package.scala
@@ -2,8 +2,8 @@ package com.wajam.nrv.extension
 
 import com.wajam.nrv.service.{Action, Service}
 import com.wajam.nrv.data._
-import com.wajam.nrv.data.MString
 import com.wajam.nrv.data.MList
+import com.wajam.nrv.InvalidParameter
 
 /**
  * The resource package defines traits and classes that are useful when creating a NRV service based on
@@ -70,24 +70,66 @@ package object resource {
 
     def token = request.token
 
-    def getParamValues(name: String): Option[Seq[String]] = {
-      request.parameters.get(name).map {
-        case MList(strSeq) if strSeq.forall(_.isInstanceOf[MString]) => strSeq.map(_.asInstanceOf[MString].value).toSeq
-        case MString(str) => Seq(str)
-        case x => throw new RuntimeException("Parameter value has an unsupported type: " + x.getClass)
+    def paramString(param: String): String = {
+      paramString(param, throw new InvalidParameter(s"Parameter $param must be specified"))
+    }
+
+    def paramString(param: String, default: => String): String = paramOptionalString(param).getOrElse(default)
+
+    def paramOptionalString(param: String): Option[String] = paramOptionalSeqString(param) match {
+      case Some(head :: _) => Some(head)
+      case _ => None
+    }
+
+    def paramOptionalSeqString(param: String): Option[Seq[String]] = request.parameters.get(param).map {
+      case MList(values) => values.map(_.toString).toSeq
+      case value: MValue => Seq(value.toString)
+      case value => throw new InvalidParameter(s"Parameter $param unsupported value $value")
+    }
+
+    def paramBoolean(param: String): Boolean = {
+      paramBoolean(param, throw new InvalidParameter(s"Parameter $param must be specified"))
+    }
+
+    def paramBoolean(param: String, default: => Boolean): Boolean = paramOptionalBoolean(param).getOrElse(default)
+
+    def paramOptionalBoolean(param: String): Option[Boolean] = paramOptionalString(param).map {
+      case "1" => true
+      case "0" => false
+      case "true" => true
+      case "false" => false
+      case "t" => true
+      case "f" => false
+      case value => throw new InvalidParameter(s"Parameter $param unsupported value $value")
+    }
+
+    def paramLong(param: String): Long = {
+      paramLong(param, throw new InvalidParameter(s"Parameter $param must be specified"))
+    }
+
+    def paramLong(param: String, default: => Long): Long = paramOptionalLong(param).getOrElse(default)
+
+    def paramOptionalLong(param: String): Option[Long] = {
+      try {
+        paramOptionalString(param).map(_.toLong)
+      } catch {
+        case e: Exception => throw new InvalidParameter(s"Parameter $param must be numeric")
       }
     }
 
-    def getParamValue(name: String): Option[String] = {
-      getParamValues(name).flatMap(_.headOption)
+    def paramInt(param: String): Int = {
+      paramInt(param, throw new InvalidParameter(s"Parameter $param must be specified"))
     }
 
-    def getParamValue(name: String, default: String): String = {
-      getParamValue(name).getOrElse(default)
-    }
+    def paramInt(param: String, default: => Int): Int = paramOptionalInt(param).getOrElse(default)
 
-    def getParamValue[DefaultType](name: String, f: String => DefaultType, default: DefaultType): DefaultType = {
-      getParamValue(name).map(f).getOrElse(default)
+    def paramOptionalInt(param: String): Option[Int] = {
+      val value = paramOptionalLong(param)
+      try {
+        value.map(_.toInt)
+      } catch {
+        case e: Exception => throw new InvalidParameter(s"Parameter $param unsupported value $value")
+      }
     }
 
     def respond(response: Any, headers: Map[String, MValue] = Map(), code: Int = 200) {
