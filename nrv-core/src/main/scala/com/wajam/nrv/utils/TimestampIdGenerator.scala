9971c59b811ee0712bdb84b25f5d6936031012ce
Manuel Laflamme <manuel@waj.am>
2014-05-14 14:50:37 -0400

    Deprecated TimestampIdGenerator now in scala-commons repo

--- nrv-core/src/main/scala/com/wajam/nrv/utils/TimestampIdGenerator.scala
+++ nrv-core/src/main/scala/com/wajam/nrv/utils/TimestampIdGenerator.scala
@@ -1,29 +1,9 @@
 package com.wajam.nrv.utils
 
-import com.wajam.commons.{CurrentTime, IdGenerator}
-
 /**
  * Generate unique long id based on the current time. Can generate up to 10K unique id if invoked in
  * the same millisecond. This class is not thread safe and must be invoked from a single thread, synchronized
  * externally or used with SynchronizedIdGenerator
  */
-class TimestampIdGenerator extends IdGenerator[Long] with CurrentTime {
-
-  private var seed: Long = currentTime
-  private var seedIndex = 0
-
-  def nextId = {
-    val newSeed = currentTime
-    if (newSeed == seed) {
-      seedIndex += 1
-    } else {
-      seed = newSeed
-      seedIndex = 0
-    }
-
-    if (seedIndex > 9999)
-      throw new IndexOutOfBoundsException
-
-    seed * 10000 + seedIndex
-  }
-}
+@deprecated(message = "Use com.wajam.commons.TimestampIdGenerator", since = "2014-05-14 14:37:31")
+class TimestampIdGenerator extends com.wajam.commons.TimestampIdGenerator
